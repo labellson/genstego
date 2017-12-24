07a929ca686d0b8eafb50f7ffb4504b73463dea2
Daniel Laguna <labellson@gmail.com>
2017-12-24 18:46:10 +0100

    Changed from iter to flattened arrays

--- scanner.py
+++ scanner.py
@@ -1,76 +1,48 @@
 from enum import Enum
 import numpy as np
 
-class Scanner():
-    """Acts like an iterator for the stego images.
-    
-    scan method returns a pixel sequence given the starting point and 
-    direction.
+class MatScanner:
+    """Returns a 1-Dimensional pixel sequence giving the starting point and
+    direaction.
     """
 
     Direction = Enum('Direction', 'raster')
-    
-    def __init__(self, img, start_point, direction):
-        """Args:
-        	start_point [x, y]: starting pixel of the scanner
-        	direction: scan direction
+
+    @staticmethod
+    def _raster_scan(mat, y, x, shape):
+        """Scans in raster order. From Left to Right. From Top to Down.
+        
+        Args:
+        	mat: matrix
+        	y: starting row 
+        	x: starting column
+        	shape: matrix shape
+        
+        Return:
+        	np.array: the flattened array from the starting point
         """
-        self._start_point = start_point
-        self._started = False
+        idx = y * shape[1] + x
+        return np.append(mat.flatten()[idx:], mat.flatten()[:idx])
 
-        self.finished = False
-        self.img = img
-        self.current = start_point
-        self.direction = direction
-
-    def scan(self):
-        """This method return the pixel index sequence given the starting
+    @classmethod
+    def scan(cls, img, y, x, direction):
+        """This method return the flattened pixel sequence given the starting
         point and direction.
        
         Args:
-        	start_point (x, y): starting pixel of the scanner
+        	img: raw image (np.array)
+        	y: starting row 
+        	x: starting column
         	direction:
 
         Return:
         	numpy.array
         """
-        if self.finished:
-            return None
+        if direction == cls.Direction.raster:
+            return cls._raster_scan(img, y, x, img.shape)
 
-        buff = self._raster_scan(self.img.shape)
 
-        if not self._started:
-            self._started = True
-
-        return buff
-
-    def _raster_scan(self, s):
-        """Scans in raster order. From Left to Right. From Top to Down.
-        
-        Args:
-        	s: shape of img
-        
-        Return:
-        	np.array: the row in case that current column is 0, or the remaining
-        		row if not.
-        """
-        # Finish condition for the iterator
-        if self._started and self.current[0] == self._start_point[0]:
-            self.finished = True
-            return self.img[self.current[0],
-                            self.current[1] : self._start_point[1]]
-            
-        # Normal raster scan
-        row = self.img[self.current[0], self.current[1]:]
-
-        # Increment the current index on raster scan
-        if self.current[0] == s[0] - 1:
-            self.current = [0, 0]
-        else:
-            self.current = [self.current[0] + 1, 0]
-
-        # Another finish condition for the iterator
-        if self._started and self.current == self._start_point:
-            self.finished = True
-
-        return row
+if __name__ == '__main__':
+    direction = MatScanner.Direction.raster
+    mat = np.arange(10).reshape(5,2)
+    print(MatScanner.scan(mat, 4, 1, direction))
