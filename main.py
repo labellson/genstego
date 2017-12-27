161f865183e8d548cfca1ecc9523e4369cc608e3
Daniel Laguna <labellson@gmail.com>
2017-12-27 20:32:19 +0100

    Created embed and fitness methods in main

--- main.py
+++ main.py
@@ -5,6 +5,20 @@ from bitarray import bitarray
 from scanner import MatScanner
 from embedder import Embedder
 
+def embed(stego, secret, chromosome):
+    # Convert to a flattened pixel sequence
+    stego = MatScanner.scan(stego, chromosome[2], chromosome[1], chromosome[0])
+    secret = secret.flatten()
+    return Embedder.embed(stego, secret, chromosome)
+
+def fitness(stego, secret, chromosome):
+    # Embed the secret sequence
+    try:
+        stego = embed(stego, secret, chromosome)
+    except:
+        return 0
+    pass
+
 # Inputs
 stego = np.arange(9).reshape(3,3)
 secret = np.array([5, 3])
@@ -17,3 +31,4 @@ stego_embedded = Embedder.embed(stego_sequence, secret, np.array([0, 0, 0, 3, 1,
 
 print('- Stego: \n{}'.format(stego))
 print('\n- Embedded: {}'.format(stego_embedded))
+
