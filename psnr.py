a9d9e670fb0fb972e07cc340eccc4062a7b99203
Daniel Laguna <labellson@gmail.com>
2018-01-01 20:42:19 +0100

    BIGFIX: psnr function returns lot of 100db, because the matrices were uint8

--- psnr.py
+++ psnr.py
@@ -3,6 +3,13 @@ import math
 
 def psnr(img1, img2):
     """Computes psnr fitness function"""
+    # Change the format of the matrix
+    if img1.dtype != (np.float32 or np.float64):
+        img1 = img1.astype(np.float32)
+
+    if img2.dtype != (np.float32 or np.float64):
+        img2 = img2.astype(np.float32)
+    
     mse = np.mean((img1 - img2)**2)
 
     if mse == 0:
